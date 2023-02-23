2c1f4e28997696d01245b840b35ac895c7e1cce6
Manelitu <emanuelwork1@gmail.com>
2023-02-23 17:49:46 -0300

    test: ensure that the content will be created

---
+++ src/app/entities/content.spec.ts
@@ -0,0 +1,16 @@
+import { Content } from './content';
+
+describe('Content entity', () => {
+  it('should be able to create a ntofication content', () => {
+    const content = new Content('any content');
+    expect(content).toBeTruthy();
+  });
+
+  it('should not be able to create a notification content with less than 5 characteres', () => {
+    expect(() => new Content('any')).toThrow();
+  });
+
+  it('should not be able to create a notification content with more than 240 characteres', () => {
+    expect(() => new Content('.'.repeat(241))).toThrow();
+  });
+});
